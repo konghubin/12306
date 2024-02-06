1da38050a70bcdf45d0b3390baea701a2d322c3b
kong <1400126414@qq.com>
2024-02-07 00:47:42 +0800

    集成校验框架Validation

--- train/member/src/main/java/com/hubindeveloper/train/member/controller/MemberController.java
+++ train/member/src/main/java/com/hubindeveloper/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.hubindeveloper.train.common.resp.CommonResp;
 import com.hubindeveloper.train.member.req.MemberRegisterReq;
 import com.hubindeveloper.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         long registerId = memberService.register(req);
         CommonResp<Long> commonResp = new CommonResp<>();
         commonResp.setContent(registerId);
--- train/member/src/main/java/com/hubindeveloper/train/member/req/MemberRegisterReq.java
+++ train/member/src/main/java/com/hubindeveloper/train/member/req/MemberRegisterReq.java
@@ -1,5 +1,6 @@
 package com.hubindeveloper.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberRegisterReq {
+    @NotBlank(message = "手机号不能为空！")
     private String mobile;
 }
