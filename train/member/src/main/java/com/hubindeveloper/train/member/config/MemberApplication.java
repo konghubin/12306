d081c15f80fc5e414f2b9708c19e2fbb67f4c440
kong <1400126414@qq.com>
2024-02-06 16:19:35 +0800

    初始化member模块

--- train/member/src/main/java/com/hubindeveloper/train/member/config/MemberApplication.java
+++ train/member/src/main/java/com/hubindeveloper/train/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package com.hubindeveloper.train.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.hubindeveloper")
+@Slf4j
 public class MemberApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("启动成功！");
+		log.info("测试地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
 	}
 
 }
