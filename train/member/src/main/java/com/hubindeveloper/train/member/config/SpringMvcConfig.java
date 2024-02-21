3c43c21b9d75393a812396b3d3860a04ef7aeb85
kong <1400126414@qq.com>
2024-02-21 23:10:09 +0800

    前端二级路由页面开发

--- train/member/src/main/java/com/hubindeveloper/train/member/config/SpringMvcConfig.java
+++ train/member/src/main/java/com/hubindeveloper/train/member/config/SpringMvcConfig.java
@@ -10,15 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//   @Resource
-//   LogInterceptor logInterceptor;
-
    @Resource
    MemberInterceptor memberInterceptor;
 
    @Override
    public void addInterceptors(InterceptorRegistry registry) {
-//       registry.addInterceptor(logInterceptor);
 
        // 路径不要包含context-path
        registry.addInterceptor(memberInterceptor)
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -12,7 +12,18 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/MainView.vue'),
     meta: {
       loginRequire: true
-    }
+    },
+    children: [{
+      path: 'welcome',
+      component: () => import('../views/main/welcome.vue')
+    }, {
+      path: 'passenger',
+      component: () => import('../views/main/passenger.vue')
+    }]
+  },
+  {
+    path: '',
+    redirect: 'welcome'
   }
 ]
 
