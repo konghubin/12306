b310f3a2a222f3d4aef6e226fb8733e8ce3ccd9e
kong <1400126414@qq.com>
2024-03-25 10:00:37 +0800

    在乘客查询返回类中增加id以及memberId属性，并用JsonSerialize解决Long精度丢失问题。

--- train/member/src/main/java/com/hubindeveloper/train/member/resp/PassengerQueryResp.java
+++ train/member/src/main/java/com/hubindeveloper/train/member/resp/PassengerQueryResp.java
@@ -1,6 +1,8 @@
 package com.hubindeveloper.train.member.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,12 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PassengerQueryResp {
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long memberId;
+
     private String name;
 
     private String idCard;
