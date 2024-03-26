0f8e663a248f7a643a9b6a89602f303c4c37a836
kong <1400126414@qq.com>
2024-03-26 14:59:46 +0800

    使用DbUtil读取表字段信息。

--- train/generator/src/main/java/com/hubindeveloper/train/generator/server/ServerGenerator.java
+++ train/generator/src/main/java/com/hubindeveloper/train/generator/server/ServerGenerator.java
@@ -1,5 +1,7 @@
 package com.hubindeveloper.train.generator.server;
 
+import com.hubindeveloper.train.generator.util.DbUtil;
+import com.hubindeveloper.train.generator.util.Field;
 import com.hubindeveloper.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -10,7 +12,9 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @description：
@@ -24,7 +28,7 @@ public class ServerGenerator {
         new File(serverPath).mkdirs();
     }
 
-    public static void main(String[] args) throws IOException, TemplateException, DocumentException {
+    public static void main(String[] args) throws Exception {
         String generatorPath = getGenerator();
         String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
         serverPath = serverPath.replace("[module]", module);
@@ -32,14 +36,27 @@ public class ServerGenerator {
         // 读取table节点
         Document document = new SAXReader().read("train/generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
-        System.out.println(table);
+//        System.out.println(table);
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
-        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+//        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
 
         String Domain = domainObjectName.getText();
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
         String do_main = tableName.getText().replaceAll("_", "-");
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
 
         // 组装参数
         Map<String, Object> param = new HashMap<>();
@@ -47,6 +64,8 @@ public class ServerGenerator {
         param.put("Domain", Domain);
         param.put("domain", domain);
         param.put("do_main", do_main);
+        param.put("tableNameCn", tableNameCn);
+        param.put("fieldList", fieldList);
         System.out.println(param);
 
         gen(Domain, param, "service");
