871fef5c3001c77287147e9b12fe72470396ffa3
kong <1400126414@qq.com>
2024-03-30 16:21:49 +0800

    快速生成火车车厢基础数据管理功能。

---
+++ train/business/src/main/java/com/hubindeveloper/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package com.hubindeveloper.train.business.req;
+
+import com.hubindeveloper.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
---
+++ train/business/src/main/java/com/hubindeveloper/train/business/service/TrainCarriageService.java
@@ -0,0 +1,59 @@
+package com.hubindeveloper.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.hubindeveloper.train.common.resp.PageResp;
+import com.hubindeveloper.train.common.util.SnowUtil;
+import com.hubindeveloper.train.business.domain.TrainCarriage;
+import com.hubindeveloper.train.business.domain.TrainCarriageExample;
+import com.hubindeveloper.train.business.mapper.TrainCarriageMapper;
+import com.hubindeveloper.train.business.req.TrainCarriageQueryReq;
+import com.hubindeveloper.train.business.req.TrainCarriageSaveReq;
+import com.hubindeveloper.train.business.resp.TrainCarriageQueryResp;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrainCarriageService {
+    @Resource
+    private TrainCarriageMapper trainCarriageMapper;
+    public void save(TrainCarriageSaveReq req){
+        DateTime now = DateTime.now();
+        TrainCarriage trainCarriage = BeanUtil.copyProperties(req, TrainCarriage.class);
+        if(ObjectUtil.isNull(trainCarriage.getId())){
+            trainCarriage.setId(SnowUtil.getSnowflakeNextId());
+            trainCarriage.setCreateTime(now);
+            trainCarriage.setUpdateTime(now);
+            trainCarriageMapper.insert(trainCarriage);
+        } else{
+            trainCarriage.setUpdateTime(now);
+            trainCarriageMapper.updateByPrimaryKey(trainCarriage);
+        }
+    }
+
+    public PageResp<TrainCarriageQueryResp> queryList(TrainCarriageQueryReq req){
+        TrainCarriageExample trainCarriageExample = new TrainCarriageExample();
+        trainCarriageExample.setOrderByClause("id desc");
+        TrainCarriageExample.Criteria criteria = trainCarriageExample.createCriteria();
+
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<TrainCarriage> trainCarriageList = trainCarriageMapper.selectByExample(trainCarriageExample);
+        PageInfo<TrainCarriage> pageInfo = new PageInfo<>(trainCarriageList);
+
+        List<TrainCarriageQueryResp> list = BeanUtil.copyToList(trainCarriageList, TrainCarriageQueryResp.class);
+        PageResp<TrainCarriageQueryResp> pageResp = new PageResp<>();
+        pageResp.setList(list);
+        pageResp.setTotal(pageInfo.getTotal());
+        return pageResp;
+    }
+
+    public void delete(Long id){
+        trainCarriageMapper.deleteByPrimaryKey(id);
+    }
+}
