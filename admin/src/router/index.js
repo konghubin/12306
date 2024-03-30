8daa05ba8a104c7f9188f496e16a03cfcd4187be
kong <1400126414@qq.com>
2024-03-30 16:41:24 +0800

    集成pinyin-pro，自动生成站名拼音及拼音首字母。

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -20,6 +20,12 @@ const routes = [{
     }, {
       path: 'train-station',
       component: () => import('../views/main/business/train-station.vue'),
+    }, {
+      path: 'train-carriage',
+      component: () => import('../views/main/business/train-carriage.vue'),
+    }, {
+      path: 'train-seat',
+      component: () => import('../views/main/business/train-seat.vue'),
     }]
   }]
 }, {
