5dbe5187b3d44f75c2a7b3acb325a8896c2b8e95
kong <1400126414@qq.com>
2024-04-02 18:35:37 +0800

    项目中增加batch定时调度模块。

---
+++ train/batch/src/main/java/com/hubindeveloper/train/batch/config/BatchApplication.java
@@ -0,0 +1,23 @@
+package com.hubindeveloper.train.batch.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.hubindeveloper")
+@Slf4j
+@MapperScan("com.hubindeveloper.train.*.mapper")
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(BatchApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("启动成功！");
+		log.info("测试地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+	}
+
+}
---
+++ train/batch/src/main/java/com/hubindeveloper/train/batch/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.hubindeveloper.train.batch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description：
+ * @author：Kong
+ * @date：2024/2/6
+ */
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
