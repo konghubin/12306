a902e111dd2326e84f5b8fa92c88129d4f34bab1
kong <1400126414@qq.com>
2024-04-03 18:17:53 +0800

    增加生成每日车厢功能。

--- train/business/src/main/java/com/hubindeveloper/train/business/service/DailyTrainCarriageService.java
+++ train/business/src/main/java/com/hubindeveloper/train/business/service/DailyTrainCarriageService.java
@@ -1,29 +1,34 @@
 package com.hubindeveloper.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.hubindeveloper.train.business.domain.*;
 import com.hubindeveloper.train.business.enums.SeatColEnum;
 import com.hubindeveloper.train.common.resp.PageResp;
 import com.hubindeveloper.train.common.util.SnowUtil;
-import com.hubindeveloper.train.business.domain.DailyTrainCarriage;
-import com.hubindeveloper.train.business.domain.DailyTrainCarriageExample;
 import com.hubindeveloper.train.business.mapper.DailyTrainCarriageMapper;
 import com.hubindeveloper.train.business.req.DailyTrainCarriageQueryReq;
 import com.hubindeveloper.train.business.req.DailyTrainCarriageSaveReq;
 import com.hubindeveloper.train.business.resp.DailyTrainCarriageQueryResp;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
+@Slf4j
 public class DailyTrainCarriageService {
     @Resource
     private DailyTrainCarriageMapper dailyTrainCarriageMapper;
+    @Resource
+    private TrainCarriageService trainCarriageService;
+
     public void save(DailyTrainCarriageSaveReq req){
         DateTime now = DateTime.now();
 
@@ -67,5 +72,31 @@ public class DailyTrainCarriageService {
 
     public void delete(Long id){
         dailyTrainCarriageMapper.deleteByPrimaryKey(id);
+    }
+
+    public void genDaily(Date date, String trainCode){
+        // 删除某日某车次的车厢信息
+        DailyTrainCarriageExample dailyTrainCarriageExample = new DailyTrainCarriageExample();
+        DailyTrainCarriageExample.Criteria criteria = dailyTrainCarriageExample.createCriteria();
+        criteria.andDateEqualTo(date).andTrainCodeEqualTo(trainCode);
+        dailyTrainCarriageMapper.deleteByExample(dailyTrainCarriageExample);
+
+        // 查出某车次的所有车厢信息
+        List<TrainCarriage> trainCarriages = trainCarriageService.selectByTrainCode(trainCode);
+
+        if(CollUtil.isEmpty(trainCarriages)){
+            log.info("该车次没有车厢基础数据！");
+            return;
+        }
+        // 生成某日某车次的车厢信息
+        for(TrainCarriage trainCarriage : trainCarriages){
+            DateTime now = DateTime.now();
+            DailyTrainCarriage dailyTrainCarriage = BeanUtil.copyProperties(trainCarriage, DailyTrainCarriage.class);
+            dailyTrainCarriage.setDate(date);
+            dailyTrainCarriage.setId(SnowUtil.getSnowflakeNextId());
+            dailyTrainCarriage.setCreateTime(now);
+            dailyTrainCarriage.setUpdateTime(now);
+            dailyTrainCarriageMapper.insert(dailyTrainCarriage);
+        }
     }
 }
--- train/business/src/main/java/com/hubindeveloper/train/business/service/DailyTrainService.java
+++ train/business/src/main/java/com/hubindeveloper/train/business/service/DailyTrainService.java
@@ -34,6 +34,8 @@ public class DailyTrainService {
     private TrainService trainService;
     @Resource
     private DailyTrainStationService dailyTrainStationService;
+    @Resource
+    private DailyTrainCarriageService dailyTrainCarriageService;
 
     public void save(DailyTrainSaveReq req){
         DateTime now = DateTime.now();
@@ -104,7 +106,9 @@ public class DailyTrainService {
         dailyTrain.setUpdateTime(now);
         dailyTrainMapper.insert(dailyTrain);
 
-        // 生产该车次的车站数据
+        // 生成该车次的车站数据
         dailyTrainStationService.genDaily(date, train.getCode());
+        // 生成该车次的车车厢数据
+        dailyTrainCarriageService.genDaily(date, train.getCode());
     }
 }
