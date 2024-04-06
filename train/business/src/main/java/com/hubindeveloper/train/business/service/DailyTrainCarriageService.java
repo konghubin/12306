acdf6e9f2f443d44782512f1add8584839d24608
kong <1400126414@qq.com>
2024-04-06 17:30:00 +0800

    后端增加实现确认订单并选座功能。

--- train/business/src/main/java/com/hubindeveloper/train/business/service/DailyTrainCarriageService.java
+++ train/business/src/main/java/com/hubindeveloper/train/business/service/DailyTrainCarriageService.java
@@ -99,4 +99,13 @@ public class DailyTrainCarriageService {
             dailyTrainCarriageMapper.insert(dailyTrainCarriage);
         }
     }
+
+    public List<DailyTrainCarriage> selectBySeatType (Date date, String trainCode, String seatType) {
+        DailyTrainCarriageExample example = new DailyTrainCarriageExample();
+        example.createCriteria()
+                .andDateEqualTo(date)
+                .andTrainCodeEqualTo(trainCode)
+                .andSeatTypeEqualTo(seatType);
+        return dailyTrainCarriageMapper.selectByExample(example);
+    }
 }
