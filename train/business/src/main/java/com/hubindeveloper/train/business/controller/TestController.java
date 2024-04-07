f5d9d4443c00e67a02ab14e9da365116d678f41d
kong <1400126414@qq.com>
2024-04-07 16:37:29 +0800

    配置FeignNacos支持应用名远程调用。

--- train/business/src/main/java/com/hubindeveloper/train/business/controller/TestController.java
+++ train/business/src/main/java/com/hubindeveloper/train/business/controller/TestController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello Business!";
     }
 }
