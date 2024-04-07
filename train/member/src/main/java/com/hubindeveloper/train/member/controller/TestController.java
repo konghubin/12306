84225fb50aa8d1873686abb418afb1b45d94f5d6
kong <1400126414@qq.com>
2024-04-07 15:23:56 +0800

    集成Nacos，并演示配置中心。

--- train/member/src/main/java/com/hubindeveloper/train/member/controller/TestController.java
+++ train/member/src/main/java/com/hubindeveloper/train/member/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.hubindeveloper.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date：2024/2/6
  */
 @RestController
+@RefreshScope
 public class TestController {
+    @Value("${test.nacos}")
+    private String name;
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello " + name + "!";
     }
 }
